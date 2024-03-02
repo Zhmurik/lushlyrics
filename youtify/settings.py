42803e4ae3e806941ba26b6a4fcff52c3278c266
zhmurik <azhmurkova@gmail.com>
2024-03-01 22:29:22 -0800

    Change settings.py DEBUG

--- youtify/settings.py
+++ youtify/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2rue53h9#2220t(1c$tx)&-2=*i0n138ug5)5lq$17)vpi#4at'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
